967b2cede7315144dee10eecc73acb559f64802a
jcrm <jcruizm22@gmail.com>
2024-02-08 20:14:36 +0100

    test: unit test

---
+++ test/helloWorld.test.js
@@ -0,0 +1,12 @@
+
+// import { describe, expect, it } from '@jest/globals';
+import request from 'supertest';
+import app from '../src/app.js';
+
+describe('GET /', () => {
+  it('responds with Hello World', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('Hello World');
+  });
+});
