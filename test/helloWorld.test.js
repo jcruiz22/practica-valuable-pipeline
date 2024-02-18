70d705a2bb3990c966ab2044287f22f796645c16
jcrm <jcruizm22@gmail.com>
2024-02-18 12:23:22 +0100

    ajuste de los test

--- test/helloWorld.test.js
+++ test/helloWorld.test.js
@@ -1,16 +1,13 @@
 
-import { describe, it, expect, afterAll } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import request from 'supertest';
-import app from '../src/server.mjs';
+import app from '../src/app.js';
 
 describe('GET /', () => {
-  it('responds with Hello World', async () => {
+  
+  it('responds with Hello World and GET method', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello World');
-  });
-
-  afterAll(() => {
-    app.close();
+    expect(response.text).toBe('Hello World!');
   });
 });
